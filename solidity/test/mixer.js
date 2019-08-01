ee7e72244032c5f0dc661fc180c759ca3c255ba8
HarryR <HarryR@noreply.users.github.com>
2019-08-01 02:02:01 +0100

    Verify proofs across native & WASM

--- solidity/test/mixer.js
+++ solidity/test/mixer.js
@@ -17,9 +17,36 @@ const PROVING_KEY_PATH = path.resolve("../.keys/mixer.pk.raw");
 const vk = require(VERIFYING_KEY_PATH);
 const { proof_to_flat, vk_to_flat } = require("../utils");
 
-const { mixer_prove, mixer_verify } = require("./helpers/libmixer");
+const { mixer_prove, mixer_prove_json, mixer_verify } = require("./helpers/libmixer");
+
+
+/**
+* If the WASM prover is available, return its `prove_json` method
+* This allows WASM proving to be dropped-in wherever native proving is
+* To ensure compatibilty between WASM vs Native builds and on-chain contracts
+*/
+function _get_wasm_prover()
+{
+    const wasm_js_file = path.resolve('../wasm/example/mixer_js.js');
+    if( fs.existsSync(wasm_js_file) )
+    {
+        const mixer_wasm = require(wasm_js_file);
+        console.log('Loaded WASM prover...');
+
+        // Load the proving key into the WASM context
+        const proving_key_data = fs.readFileSync(PROVING_KEY_PATH);
+        mixer_wasm.FS_createDataFile('/', 'mixer.pk.raw', proving_key_data, true, false, false);
+
+        const prove_json = mixer_wasm.cwrap('mixer_prove_json', 'string', ['string', 'string']);
+        const verify = mixer_wasm.cwrap('mixer_verify', 'bool', ['string', 'string']);
+
+        return { prove_json, verify };
+    }
+}
+const wasm_mixer = _get_wasm_prover();
 
 const SKIP_SLOW_TESTS = true;
+
 
 contract("Mixer", function([
   depositer1,
@@ -53,38 +80,40 @@ contract("Mixer", function([
   async function computeProof(
     _nullifier_secret,
     _leaf_index,
-    _withdrawer = withdrawer1
+    _withdrawer = withdrawer1,
+    _prover = undefined,
+    _proving_key = undefined
   ) {
+    _prover = _prover || mixer_prove_json;  // Prover can be swapped...
+    _proving_key = _proving_key || PROVING_KEY_PATH;
+
     // Compute leaf binary address
     const tree_depth = (await this.mixer.treeDepth()).toNumber();
-    const leaf_address = _leaf_index // (6)_10 = (110)_2 becomes "011 0...(24x)...0"
-      .toString(2)
-      .padStart(tree_depth, "0")
-      .split("")
-      .reverse()
-      .join("");
     // Compute merkle path neighbour hashes
     const path_neighbours = await this.mixer.getMerklePath(_leaf_index);
     // Compute merkle root
     const merkle_root = await this.mixer.getRoot();
     // Compute nullifier
     const nullifier = await this.mixer.makeNullifierHash(_nullifier_secret);
+
     // Generate proof
-    let args = [
-      PROVING_KEY_PATH,
-      merkle_root.toString(10),
-      toBN(_withdrawer).toString(10),
-      nullifier.toString(10),
-      _nullifier_secret.toString(10),
-      leaf_address,
-      path_neighbours.map(h => h.toString(10))
-    ];
-    const proof_json = mixer_prove(...args);
+    let args_json = JSON.stringify({
+      "root": merkle_root.toString(10),
+      "wallet_address": toBN(_withdrawer).toString(10),
+      "nullifier": nullifier.toString(10),
+      "nullifier_secret": _nullifier_secret.toString(10),
+      "address": _leaf_index.toNumber(),
+      "path": path_neighbours.map(h => h.toString(10))
+    });
+    console.log('Input proof args (json):', args_json);
+    const proof_json = _prover(_proving_key, args_json);
+
     assert.notEqual(
       proof_json,
       null,
       "Failed to build valid proof (invalid proof inputs)"
     );
+    console.log('Output proof result (json)', proof_json);
 
     return { proof_json, nullifier, merkle_root };
   }
@@ -93,8 +122,10 @@ contract("Mixer", function([
     _proof_json,
     _nullifier,
     _merkle_root,
-    _withdrawer = withdrawer1
+    _withdrawer = withdrawer1,
+    _verifier = undefined
   ) {
+    _verifier = _verifier || mixer_verify;
     const proof = JSON.parse(_proof_json);
 
     // Ensure proof inputs match our public variables
@@ -109,11 +140,11 @@ contract("Mixer", function([
 
     // Verify proof using native library
     // XXX: node-ffi on OSX will not null-terminate strings returned from `readFileSync` !
-    const proof_valid_native = mixer_verify(
+    const proof_valid_native = _verifier(
       fs.readFileSync(VERIFYING_KEY_PATH) + "\0",
       _proof_json
     );
-    assert.isTrue(proof_valid_native);
+    assert.isTrue(proof_valid_native === true || proof_valid_native === 1);
 
     // Verify proof using Verifier contract
     const proof_valid_contract = await this.mixer.verifyProof(
@@ -134,11 +165,31 @@ contract("Mixer", function([
       expect(mixerAfterD.sub(mixerBeforeD)).to.eq.BN(AMOUNT);
 
       // Compute and verify the proof
-      const { proof_json, nullifier, merkle_root } = await computeProof(
+      let { proof_json, nullifier, merkle_root } = await computeProof(
         nullifier_secret,
         leaf_index
       );
       await verifyProof(proof_json, nullifier, merkle_root);
+
+      if( wasm_mixer ) {
+        console.log('Testing native proof with WASM verifier');
+        await verifyProof(proof_json, nullifier, merkle_root, withdrawer1, wasm_mixer.verify);
+
+        console.log('Testing WASM prover');
+        let proof_wasm = await computeProof(
+          nullifier_secret,
+          leaf_index,
+          withdrawer1,
+          wasm_mixer.prove_json,
+          '/mixer.pk.raw'
+        );
+
+        console.log('Verify WASM proof with WASM verifier');
+        await verifyProof(proof_wasm.proof_json, proof_wasm.nullifier, proof_wasm.merkle_root, withdrawer1, wasm_mixer.verify);
+
+        console.log('Verify WASM proof with Native verifier');
+        await verifyProof(proof_wasm.proof_json, proof_wasm.nullifier, proof_wasm.merkle_root);
+      }
 
       // Verify nullifier doesn't exist
       let is_nullifier_spent = await this.mixer.isSpent(nullifier);
